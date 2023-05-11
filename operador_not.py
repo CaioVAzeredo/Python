a486b04f81dcaff8dc9f35be502d2435cb09b360
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-05-11 12:09:27 -0300

    exercicio

--- operador_not.py
+++ operador_not.py
@@ -9,14 +9,10 @@
 # Também existe o tipo None que é
 # usado para representar um não valor
 
-entrada = input("[E]ntrar [S]air: ")
-senha_digitada = input("Senha: ")
-
-senha_permitida = "123456"
-
-if entrada == "E" and senha_digitada == senha_permitida:
-    print("Entrar")
-else:
-    print("Sair")
-
-# Avaliação de curto circuito
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+# senha = input('Senha: ')
+print(not True)  # False
+print(not False)  # True
