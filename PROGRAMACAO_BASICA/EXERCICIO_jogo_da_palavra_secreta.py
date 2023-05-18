0a12af8e0cba838603bcb21869cb9cea1c2feec4
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-05-17 23:34:09 -0300

    primeira parte feita

--- PROGRAMACAO_BASICA/EXERCICIO_jogo_da_palavra_secreta.py
+++ PROGRAMACAO_BASICA/EXERCICIO_jogo_da_palavra_secreta.py
@@ -14,3 +14,20 @@ na palavra secreta.
 Faça a contagem de tentativas do seu
 usuário.
 """
+palavra_secreta = "Caio".lower()
+letras_acertadas = ""
+
+while True:
+    letra_digitada = input("Insira uma letra: ")
+
+    if len(letra_digitada) > 1:
+        print("Digite apenas uma letra.")
+        continue
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            print(letra_secreta)
+        else:
+            print("*")
