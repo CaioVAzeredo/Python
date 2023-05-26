bbca12839ccf293ca01aea14634500aa29badf4a
CaioVAzeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-05-26 15:48:09 -0300

    exercicio cpf feito

--- PROGRAMACAO_BASICA/EXERCICIO_validador_de_cpf.py
+++ PROGRAMACAO_BASICA/EXERCICIO_validador_de_cpf.py
@@ -1,4 +1,4 @@
-
+import time
 #    0    4    8   2   6   9   2   4     1
 # *
 #   10    9    8   7   6   5   4   3     2
@@ -15,24 +15,51 @@
 #se maior que 9:
     #o numero deve ser 0(zero)
 
+while True:
+    condicao = input('Deseja coloca um cpf? (s-sim. n-não):\n')
+    if condicao == 's' or condicao == 'S':
+        cpf = input("Insira um numero de cpf: ")
+        digito_1 = cpf[:9] #Limitando do 0(zero) até o 9
 
-numeros_para_Inserir = input("Insira um numero de cpf: ")
-numeros = numeros_para_Inserir[:9] #Limitando do 0(zero) até o 9
+        cont_10 = 10
 
+        soma_1 = 0
+        # digito 1
+        for numero in digito_1:
+            soma_1 += int(numero) * cont_10
+            cont_10 -= 1
+            
+        resto_1 = (soma_1 * 10) % 11
+        if resto_1 <=9:
+            resto_1 = resto_1
 
-        #0 1 2 3 4 5 6 7 8
-cont = 10
-soma = 0
+        else:
+            resto_1 = 0
 
-for numero in numeros:
-    soma += int(numero) * cont
-    cont -= 1
-    
-resto = (soma * 10) % 11
-if resto <=9:
-    resto = resto
-    print(resto)
-else:
-    resto = 0
-    print(resto)
-""" print((10*217)% 11) """
+        #digito 2
+
+        digito_2 = cpf[:10]
+        soma_2 = 0
+        cont_11 = 11
+        for numero in digito_2:
+            soma_2 += int(numero) * cont_11
+            cont_11 -= 1
+
+        resto_2 = (soma_2*10)%11
+        if resto_2  <= 9:
+            resto_2 = resto_2
+
+        else:
+            resto_2 = 0
+
+        if cpf[9:12] == (str(resto_1) + str(resto_2)):
+            print('CPF válido')
+        else:
+            print('CPF NÃO é válido')
+            """ print((10*217)% 11) """
+    elif condicao == 'n' or condicao == 'N':
+        break
+    else:
+        print('Insira um valor válido')
+print('Fim do programa')
+time.sleep(5)
